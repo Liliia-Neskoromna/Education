0ebc642671835bf5b7c655159e76df7aea55c89c
Liliia Neskoromna <>
2020-12-07 15:30:26 +0200

    add .swiftlint.ylm

--- Education/HWOneViewController.swift
+++ Education/HWOneViewController.swift
@@ -14,12 +14,17 @@ final class HWOneViewController: UIViewController {
     @IBOutlet private weak var factorialIterButton: UIButton!
     @IBOutlet private weak var fibonacciButton: UIButton!
     @IBOutlet private weak var piToNButton: UIButton!
+    
     var stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         configureStackView()
     }
+    
     private func configureStackView() {
+        
         stackView.axis         = .vertical
         stackView.distribution = .fillEqually
         stackView.alignment    = .fill
@@ -28,7 +33,9 @@ final class HWOneViewController: UIViewController {
         setStackViewConstraint()
         addSubviewsToStackView()
     }
+    
     private func addSubviewsToStackView() {
+        
         stackView.addArrangedSubview(inputTextField)
         stackView.addArrangedSubview(outputLabel)
         stackView.addArrangedSubview(factorialRecButton)
@@ -36,11 +43,37 @@ final class HWOneViewController: UIViewController {
         stackView.addArrangedSubview(fibonacciButton)
         stackView.addArrangedSubview(piToNButton)
     }
+    
     private func setStackViewConstraint() {
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50).isActive = true
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                       constant: 50).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                                           constant: 50).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                          constant: -50).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                                            constant: -50).isActive = true
+    }
+}
+
+extension HWOneViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else {
+            return true
+        }
+        let newStr = (text as NSString).replacingCharacters(in: range, with: string)
+
+        guard let intValue = Int(newStr) else {
+            return true
+        }
+        return intValue <= 1000
     }
 }
